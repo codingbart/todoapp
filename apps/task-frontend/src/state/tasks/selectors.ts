b312f10f1a07518ecc4e8ff27e22509c93866a85
Filip Stępień <filip.stepien30@gmail.com>
2026-05-18 12:56:39 +0200

    fix: task selector

--- apps/task-frontend/src/state/tasks/selectors.ts
+++ apps/task-frontend/src/state/tasks/selectors.ts
@@ -1,8 +1,8 @@
 import { type RootState } from '@/lib/store';
 import { tasksAdapter } from './slice';
 
-export const { selectAll: selectAllTasks } = tasksAdapter.getSelectors(
-    (state: RootState) => state.tasks,
+export const { selectAll: selectAllTasks } = tasksAdapter.getSelectors<RootState>(
+    state => state.tasks
 );
 
 export const selectTasksStatus = (state: RootState) => state.tasks.status;
